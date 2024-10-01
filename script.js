5fa5315b000b396008bfa7b7cf592d69146fa7e1
harii77 <hariharanvj7777@gmail.com>
2024-10-01 19:39:33 +0530

    updated js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 "use strict";
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    const input = document.getElementById("cmd-input");
-    if (input.value === "help") {
-      alert("Diplay help...");
-    }
-  }
-});
+const input = document.getElementById("cmd-input");
+const output = document.getElementById("output");
+const asciiArt = document.getElementById("ascii-art");
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+// const input = document.getElementById("cmd-input");
+// if (input.value === "help") {
+//   alert("Diplay help...");
+// }
+//   }
+// });
+//
