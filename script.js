b109e0befb7764acc0c58b7c2667c2375c0497d2
harii77 <hariharanvj7777@gmail.com>
2024-10-07 22:59:06 +0530

    updated script

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 "use strict";
 
-const input = document.getElementById("cmd-input");
-const output = document.getElementById("output");
-const asciiArt = document.getElementById("ascii-art");
+let menuBtn = document.getElementById("myNavMenu");
 
-// document.addEventListener("keydown", function (event) {
-//   if (event.key === "Enter") {
-// const input = document.getElementById("cmd-input");
-// if (input.value === "help") {
-//   alert("Diplay help...");
-// }
-//   }
-// });
-//
+const myMenuFunction = function () {
+  if (menuBtn.className === "nav-menu") {
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+};
+
+const typingEffect = new Typed(".typedText", {
+  strings: ["Developer", "Designer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
+});
