3336ce74c601bca88c12cfccb5528fd8e9278e3a
harii77 <hariharanvj7777@gmail.com>
2024-10-08 19:55:38 +0530

    updated Script

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
-let menuBtn = document.getElementById("myNavMenu");
+const menuBtn = document.getElementById("myNavMenu");
+const navHeader = document.getElementById("header");
+const scrollBody = document.body.scrollTop;
+const scrollDocEle = document.documentElement.scrollTop;
 
 const myMenuFunction = function () {
   if (menuBtn.className === "nav-menu") {
@@ -17,3 +20,15 @@ const typingEffect = new Typed(".typedText", {
   backSpeed: 80,
   backDelay: 2000,
 });
+
+const headerShadow = function () {
+  if (scrollBody > 50 || scrollDocEle > 50) {
+    navHeader.style.boxShadow = "0 1px 6px rbga(0,0,0,0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+  } else {
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.lineHeight = "90px";
+  }
+};
